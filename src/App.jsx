a4d799992bef1cf8cf84257d962f353435e869c3
afonso <156040578+afonsotrs@users.noreply.github.com>
2024-08-01 10:45:58 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect, useRef } from 'react';
-import {Formik, Form, Field, ErrorMessage} from "formik";
+// import {Formik, Form, Field, ErrorMessage} from "formik";
 
 function App() {
   // Variáveis
