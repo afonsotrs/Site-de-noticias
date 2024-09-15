455a039c4ec47e3a45da73d69d040ba1b2c6e8ad
afonsotrs <afonsotorrescp@gmail.com>
2024-09-15 01:42:53 -0300

    ajeitando o css

--- site de noticias/client/src/componentes/TelaCadastro/TelaCadastro.jsx
+++ site de noticias/client/src/componentes/TelaCadastro/TelaCadastro.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import '../TelaLogin/TelaLogin.css'
+// import '../TelaLogin/TelaLogin.css'
 const TelaCadastro = () => {
     return(
-        <div className="container">
-        <div className="header">
-            <h1>Cadastre-se</h1>
-        </div>
-        <div className="form">
-            <input type="email" id="email" placeholder="Digite seu email..." />
-            <input type="password" id="senha" placeholder="Digite sua senha..." />
-            <button type="submit" id="bnt">Cadastre-se</button>
-        </div>
-        <div className="footer">
-            <a href="/login">Já possui uma conta? Entre aqui</a>
+        <div className="containercadastro">
+        <div className="formcadastro">
+            <div className="headercadastro">
+                <h1>Cadastre-se</h1>
+            </div>
+            <input type="email" id="email" placeholder="Digite seu email" />
+            <input type="password" id="senha" placeholder="Digite sua senha" />
+            <button type="submit" id="bntcadastro">Cadastre-se</button>
+            <div className="footercadastro">
+                <a href="/login">Já possui uma conta? Entre aqui</a>
+            </div>
         </div>
     </div>
     )
--- site de noticias/client/src/componentes/TelaLogin/TelaLogin.jsx
+++ site de noticias/client/src/componentes/TelaLogin/TelaLogin.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import './TelaLogin.css';
+// import './TelaLogin.css';
 
 const TelaLogin = () => {
     return(
-        <div className="container">
-            <div className="header">
-                <h1>Faça seu login</h1>
-            </div>
-            <div className="form">
-                <input type="email" id="email" placeholder="Digite seu email..." />
-                <input type="password" id="senha" placeholder="Digite sua senha..." />
-                <button type="submit" id="bnt">Entrar</button>
-            </div>
-            <div className="footer">
-                <a href="/cadastro">Não possui uma conta? cadastre-se aqui</a>
+        <div className="containerlogin">
+            <div className="formlogin">
+                <div className="headerlogin">
+                    <h1>Faça seu login</h1>
+                </div>
+                <input type="email" id="email" placeholder="Digite seu email" />
+                <input type="password" id="senha" placeholder="Digite sua senha" />
+                <button type="submit" id="bntlogin">Entrar</button>
+                <div className="footerlogin">
+                    <a href="/cadastro">Não possui uma conta? cadastre-se aqui</a>
+                </div>
             </div>
         </div>
     )
